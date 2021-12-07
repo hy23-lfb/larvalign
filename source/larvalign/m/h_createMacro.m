17ec9266f3260bf9574d04979a91fb12a59aa1b2
Harsha Yogeshappa <yogeshappa@lfb.rwth-aachen.de>
2021-12-07 12:38:10 +0100

    Added helper functions

--- source/larvalign/m/h_createMacro.m
+++ source/larvalign/m/h_createMacro.m
@@ -1,5 +1,15 @@
-name = ["C:\\Users\\yogeshappa\\Documents\\Files_Hiwi\\Datasets\\Segmented_Files\\d3\\d3_"];
-for i = 1:430
+%open("D:\\Harsha\\Files_Hiwi\\Datasets\\Standard_Brain\\25_Scaled_Tiff\\d4\\p\\2.tif");
+
+%name = ["C:\\Users\\yogeshappa\\Documents\\Files_Hiwi\\Datasets\\Segmented_Files\\d3\\d3_"];
+%for i = 1:430
+%    file = ["open(" + '"' + name + num2str(i) + ".tif"  + '");'];
+%    fprintf("%s\n",file);
+%end
+%closeStmt = ['run("Concatenate...", "all_open title=[Concatenated Stacks]");'];
+%fprintf("%s\n", closeStmt);
+
+name = ["D:\\Harsha\\Files_Hiwi\\Datasets\\Standard_Brain\\25_Scaled_Tiff\\d4\\p\\"];
+for i = 1:100
     file = ["open(" + '"' + name + num2str(i) + ".tif"  + '");'];
     fprintf("%s\n",file);
 end
